b7c63f55d205881160c311b03a3cab7b76caeb00
atisha2805 <atisha2027@gmail.com>
2026-05-19 22:34:15 +0530

    Exp 8

---
+++ ProjectFolder/Day-05/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <h1>Counter Application</h1>
+
+      <h2>{count}</h2>
+
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
